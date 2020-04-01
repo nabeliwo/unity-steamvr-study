7f7537cb81737cf95117948ec20916dc5378462a
nabeliwo <nabeliwo@gmail.com>
2020-04-01 21:34:17 +0900

    :sparkles: add actionTest

---
+++ Assets/Scripts/ActionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ActionsTest : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean teleportAction;
+    public SteamVR_Action_Boolean grabAction;
+
+    void Update()
+    {
+        if (GetTeleportDown())
+        {
+            print("Teleport " + handType);
+        }
+
+        if (GetGrab())
+        {
+            print("Grab " + handType);
+        }
+    }
+
+    public bool GetTeleportDown()
+    {
+        return teleportAction.GetStateDown(handType);
+    }
+
+    public bool GetGrab()
+    {
+        return grabAction.GetState(handType);
+    }
+}
---
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ControllerGrabObject : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Behaviour_Pose controllerPose;
+    public SteamVR_Action_Boolean grabAction;
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    private void SetCollidingObject(Collider col)
+    {
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+
+        collidingObject = col.gameObject;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    void Update()
+    {
+        
+    }
+}
